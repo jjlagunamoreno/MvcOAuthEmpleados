dd786c1cdf4964184f28605ca52cc8e3fe8c0bad
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-03-28 11:07:09 +0100

    Add project files.

---
+++ MvcOAuthEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,41 @@
+﻿using ApiOAuthEmpleados.Models;
+using Microsoft.AspNetCore.Mvc;
+using MvcOAuthEmpleados.Filters;
+using MvcOAuthEmpleados.Services;
+
+namespace MvcOAuthEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private ServiceEmpleados service;
+
+        public EmpleadosController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+        [AuthorizeEmpleados]
+        public async Task<IActionResult> Index()
+        {
+            List<Empleado> empleados =
+                await this.service.GetEmpleadosAsync();
+            return View(empleados);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            //TENDREMOS EL TOKEN EN SESSION
+            string token = HttpContext.Session.GetString("TOKEN");
+            if (token == null)
+            {
+                ViewData["MENSAJE"] = "Debe validarse en Login";
+                return View();
+            }
+            else
+            {
+                Empleado empleado = await
+                    this.service.FindEmpleadoAsync(id, token);
+                return View(empleado);
+            }
+        }
+    }
+}
---
+++ MvcOAuthEmpleados/Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiOAuthEmpleados.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Apellido { get; set; }
+        public string Oficio { get; set; }
+        public int Salario { get; set; }
+        public int IdDepartamento { get; set; }
+    }
+}
